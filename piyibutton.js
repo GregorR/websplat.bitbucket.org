2ad0e506fbc5526be7100264e5df7dc30bea3f81
Gregor Richards <Richards@codu.org>
2012-07-18 20:26:00 -0400

    Need \", not '.

--- piyibutton.js
+++ piyibutton.js
@@ -10,5 +10,5 @@
     st("http://websplat.bitbucket.org/piyibutton.css");
     st("http://fonts.googleapis.com/css?family=Neuton:400,700");
 
-    document.write("<div class='piyib'><a href='javascript:(function(s){s=document.createElement('script');s.src='http://websplat.bitbucket.org/websplat/loader.js';document.getElementsByTagName('head')[0].appendChild(s);})()'>Needs More Ponies</a></div>");
+    document.write("<div class='piyib'><a href=\"javascript:(function(s){s=document.createElement('script');s.src='http://websplat.bitbucket.org/websplat/loader.js';document.getElementsByTagName('head')[0].appendChild(s);})()\">Needs More Ponies</a></div>");
 })();
