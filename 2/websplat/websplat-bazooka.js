525525a73f9c585c8f2df65b0e35611b6190a774
Gregor Richards <Richards@codu.org>
2013-01-20 18:26:15 -0500

    Updated websplat2.

--- 2/websplat/websplat-bazooka.js
+++ 2/websplat/websplat-bazooka.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010, 2012-2013 Gregor Richards
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 (function() {
     var bazRad = 100;
     var gd = WebSplat.conf.gridDensity;
@@ -26,8 +42,18 @@
                 if (els === null) continue;
                 for (var i = 0; i < els.length; i++) {
                     var el = els[i];
-                    if ("wpSprite" in el) continue;
-                    if (WebSplat.elInDistance(el, bazRad, bazX, bazY)) {
+                    if ("wpSprite" in el) {
+                        // give it momentum
+                        var sprite = el.wpSprite;
+                        var dx = sprite.x - bazX;
+                        var dy = sprite.y - bazY;
+                        var dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                        if (dist < bazRad) {
+                            var angle = Math.atan((sprite.y - bazY) / (sprite.x - bazX));
+                            sprite.xvel = Math.cos(angle) * (bazRad - dist) * ((dx>0)?1:-1);
+                            sprite.yvel = Math.sin(angle) * (bazRad - dist) * ((dy>0)?1:-1);
+                        }
+                    } else if (WebSplat.elInDistance(el, bazRad, bazX, bazY)) {
                         WebSplat.remElementPosition(el);
                         el.style.visibility = "hidden";
                     }
