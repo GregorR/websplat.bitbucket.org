0d1f63ace6627f210a333688af23d489585bc244
Gregor Richards <Richards@codu.org>
2013-01-25 16:16:12 -0500

    Updated websplat2.

--- 2/websplat/websplat-goody.js
+++ 2/websplat/websplat-goody.js
@@ -1,3 +1,19 @@
+/*
+* Copyright (c) 2010, 2012-2013 Gregor Richards
+*
+* Permission to use, copy, modify, and/or distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
+*
+* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+* SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+* OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+* CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+///<reference path="websplat.ts" />
 var WebSplat;
 (function (WebSplat) {
     var pressingUp = false;
@@ -12,6 +28,7 @@ var WebSplat;
             switch(ev.which) {
                 case 37:
                 case 65: {
+                    // a
                     WebSplat.player.xacc = -1;
                     WebSplat.player.xaccmax = WebSplat.conf.moveSpeed * -1;
                     break;
@@ -19,6 +36,7 @@ var WebSplat;
                 }
                 case 39:
                 case 68: {
+                    // d
                     WebSplat.player.xacc = 1;
                     WebSplat.player.xaccmax = WebSplat.conf.moveSpeed;
                     break;
@@ -26,6 +44,7 @@ var WebSplat;
                 }
                 case 38:
                 case 87: {
+                    // w
                     if(pressingUp) {
                         break;
                     }
@@ -39,6 +58,7 @@ var WebSplat;
                 }
                 case 40:
                 case 83: {
+                    // s
                     if(WebSplat.player.on !== null) {
                         WebSplat.player.mode = "jfc";
                         WebSplat.player.frame = 0;
@@ -52,6 +72,8 @@ var WebSplat;
                 }
                 case 70:
                 case 32: {
+                    // space
+                    //player.specialOn();
                     break;
 
                 }
@@ -68,6 +90,7 @@ var WebSplat;
             switch(ev.which) {
                 case 37:
                 case 65: {
+                    // a
                     if(WebSplat.player.xacc < 0) {
                         WebSplat.player.xacc = false;
                         WebSplat.player.xaccmax = false;
@@ -77,6 +100,7 @@ var WebSplat;
                 }
                 case 39:
                 case 68: {
+                    // d
                     if(WebSplat.player.xacc > 0) {
                         WebSplat.player.xacc = false;
                         WebSplat.player.xaccmax = false;
@@ -86,17 +110,21 @@ var WebSplat;
                 }
                 case 38:
                 case 87: {
+                    // w
                     pressingUp = false;
                     break;
 
                 }
                 case 40:
                 case 83: {
+                    // s
                     break;
 
                 }
                 case 70:
                 case 32: {
+                    // space
+                    //player.specialOff();
                     break;
 
                 }
--- 2/websplat/websplat-pony.js
+++ 2/websplat/websplat-pony.js
@@ -3,6 +3,24 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
+/*
+* Copyright (c) 2010, 2012 Gregor Richards
+*
+* Permission to use, copy, modify, and/or distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
+*
+* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+* SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+* OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+* CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+///<reference path="websplat.ts" />
+//<style implicitAny="on" />
+//<style eqeqeq="on" />
 var WebSplat;
 (function (WebSplat) {
     var ponyConf = {
@@ -14,6 +32,7 @@ var WebSplat;
         edgeDetectDist: 5,
         edgeDetectSize: 10
     };
+    /* hopAbove*2 */
     var ponyImageSets = {
         r: {
             frames: 16,
@@ -40,6 +59,7 @@ var WebSplat;
             ]
         }
     };
+    // make this fit with r
     var Pony = (function (_super) {
         __extends(Pony, _super);
         function Pony() {
@@ -53,12 +73,14 @@ var WebSplat;
                 this.state = "r";
                 this.frame = 0;
             }
-        };
+        }// every tick, change the acceleration inexplicably
+        ;
         Pony.prototype.tick = function () {
             if(!this.onScreen()) {
                 return;
             }
             if(this.dead) {
+                // whoops!
                 if(this.state === "da") {
                     this.state = "db";
                     this.frame = 0;
@@ -76,11 +98,14 @@ var WebSplat;
                 this.updateImage();
                 return;
             }
+            // do a normal round
             _super.prototype.tick.call(this);
             if(WebSplat.player === this) {
                 return;
             }
+            // only do anything if we're on a platform
             if(!this.munching && this.on !== null) {
+                // if we bumped into something left or there is nothing to the left ...
                 if(this.leftOf !== null || this.noPlatform(this.x - ponyConf.edgeDetectDist - ponyConf.edgeDetectSize)) {
                     this.xacc = 1;
                     this.xaccmax = ponyConf.moveSpeed;
@@ -99,25 +124,31 @@ var WebSplat;
                 this.xacc = false;
             }
             if(this.y < 0) {
+                // don't let them go above the screen
                 this.setXY(this.x, 0);
             }
-        };
+        }// is their no platform at this X?
+        ;
         Pony.prototype.noPlatform = function (x) {
             var els = WebSplat.getElementsByBoxThru(this, this.thru, false, x, ponyConf.edgeDetectSize, this.y + this.h, ponyConf.edgeDetectSize);
             if(els === null) {
                 return true;
             }
             return false;
-        };
+        }// if we hit the bottom, go back to the top
+        ;
         Pony.prototype.hitBottom = function () {
             this.setXY(this.x, this.h * 2);
-        };
+        }// take damage
+        ;
         Pony.prototype.takeDamage = function (from, pts) {
+            // make it dead
             this.dead = true;
             this.mode = "d";
             this.state = "da";
             this.frame = 0;
             this.updateImage();
+            // then remove it
             var spthis = this;
             WebSplat.deplatformSprite(spthis);
             setTimeout(function () {
@@ -129,8 +160,10 @@ var WebSplat;
         return Pony;
     })(WebSplat.Sprite);
     WebSplat.Pony = Pony;    
+    // by default, stick some ponies in the game
     WebSplat.addHandler("postload", function () {
         var last = null;
+        // create some ponies!
         WebSplat.spritesOnPlatform(ponyImageSets.r.width, ponyImageSets.r.height, 480, 480 * 320, function () {
             return (last = new Pony());
         });
